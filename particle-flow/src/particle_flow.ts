cc8ea7ac6f73ba6c1fe51a0310207251beec0562
Langbein, Michael <michael.langbein@dlr.de>
2021-09-29 21:53:12 +0200

    feat: some more work on particle shader

--- particle-flow/src/particle_flow.ts
+++ particle-flow/src/particle_flow.ts
@@ -1,9 +1,9 @@
-import { ArrayBundle, AttributeData, Context, Program, UniformData } from '@mgx/engine1';
+import { ArrayBundle, AttributeData, Context, Program, TextureData, UniformData } from '@mgx/engine1';
 import { FeatureCollection, Point } from 'geojson';
 import Delaunator from 'delaunator';
+import { createEmptyFramebufferObject } from '@mgx/engine1/dist/engine1/src/webgl';
 
 
-// cmp https://github.com/dlr-eoc/ukis-frontend-libraries/blob/master/projects/utils-maps/src/lib/ol/customRenderers/particle_renderer.ts
 
 export class ParticleFlow {
     private bundle: ArrayBundle;
@@ -56,6 +56,71 @@ export class ParticleFlow {
         bundle.upload(context);
         bundle.initVertexArray(context);
         bundle.bind(context);
+
+
+        
+        const particleFb1 = createEmptyFramebufferObject(gl, gl.canvas.width, gl.canvas.height, 'display');
+        const particleFb2 = createEmptyFramebufferObject(gl, gl.canvas.width, gl.canvas.height, 'display');
+
+        const particleProgram = new Program(`
+            attribute vec3 a_vertex;
+            attribute vec2 a_textureCoord;
+            varying vec2 v_textureCoord;
+            void main() {
+                v_textureCoord = a_textureCoord;
+                gl_Position = vec4(a_vertex.xyz, 1.0);
+            }
+        `, `
+            precision mediump float;
+            uniform sampler2D u_forceTexture;
+            uniform sampler2D u_particleTexture;
+            uniform float u_deltaT;
+            varying vec2 v_textureCoord;
+            float rand(vec2 co){
+                return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+            }
+            void main() {
+                // moving particles
+                vec2 speed = ((texture2D(u_forceTexture, v_textureCoord) - 0.5 ) * 2.0).xy;
+                vec2 samplePoint = v_textureCoord - speed * u_deltaT * 0.1;
+                samplePoint = mod(samplePoint, 1.0);
+                gl_FragColor = texture2D(u_particleTexture, samplePoint);
+                // fade out
+                float fadeRate = 0.95;
+                if (gl_FragColor.x != 1.0) {
+                    vec4 lastColor = texture2D(u_particleTexture, v_textureCoord);
+                    vec4 fadedColor = vec4(lastColor.xyz * fadeRate, 1.0);
+                    gl_FragColor = fadedColor;
+                }
+                // spawn and die-off
+                float spawnChance = 0.0005;
+                float dieChance = 0.2;
+                float randVal = rand(v_textureCoord * abs(sin(u_deltaT)) * 0.01);
+                if (randVal > (1. - spawnChance)) {  // spawn
+                    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+                } if (randVal < dieChance) {   // die off
+                    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+                }
+                // no particles outside texture
+                if (texture2D(u_forceTexture, v_textureCoord) == vec4(0.0, 0.0, 0.0, 0.0)) {
+                    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+                }
+            }
+        `);
+
+        const particleBundle = new ArrayBundle(
+            particleProgram, 
+            {
+                'a_vertex': new AttributeData(),
+                'a_textureCoord': new AttributeData()
+            }, {
+                'u_deltaT': new UniformData()
+            }, {
+                'u_forceTexture': new TextureData(),
+                'u_particleTexture': new TextureData()
+            }, 'triangles', nrAttributes
+        );
+
 
         this.bundle = bundle;
         this.context = context;

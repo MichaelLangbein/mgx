bf98ac63e8d68b1ff12c54bdb8c46eda83ed551e
michaellangbein <michael_langbein@hotmail.de>
2021-11-29 23:52:08 +0100

    threejs-utils: moved custom material into utils.

--- threejs-tools/src/utils.ts
+++ threejs-tools/src/utils.ts
@@ -1,5 +1,10 @@
-import { DataTexture, FloatType, NearestFilter, RepeatWrapping, RGBAFormat, ShaderMaterial, WebGLRenderer, WebGLRenderTarget } from "three";
+import {
+    Color, DataTexture, FloatType, Mesh, NearestFilter, PlaneBufferGeometry, RepeatWrapping, RGBAFormat,
+    ShaderChunk, ShaderLib, ShaderMaterial, UniformsUtils, WebGLRenderer,
+    WebGLRenderTarget
+} from "three";
 import { GPUComputationRenderer } from "three/examples/jsm/misc/GPUComputationRenderer";
+import { EngineObject } from ".";
 
 
 export class RungeKuttaRenderer {
@@ -83,7 +88,7 @@ export class RungeKuttaRenderer {
     }
 
     getOutputTexture() {
-        const {dataSource, dataSink} = this.getCurrentSourceAndSink(this.i);
+        const { dataSource, dataSink } = this.getCurrentSourceAndSink(this.i);
         return dataSource.texture;
     }
 
@@ -100,7 +105,7 @@ export class RungeKuttaRenderer {
     }
 
     update() {
-        const {dataSource, dataSink} = this.getCurrentSourceAndSink(this.i);
+        const { dataSource, dataSink } = this.getCurrentSourceAndSink(this.i);
 
         this.differentialShader.uniforms.dataTexture.value = dataSource.texture;
         this.differentialShader.uniforms.dk.value = 0.0;
@@ -133,7 +138,7 @@ export class RungeKuttaRenderer {
             dataSource = this.summaryTarget2;
             dataSink = this.summaryTarget1;
         }
-        return {dataSource, dataSink};
+        return { dataSource, dataSink };
     }
 
     private initTextures(data: Float32Array) {
@@ -166,4 +171,162 @@ export class RungeKuttaRenderer {
         this.summaryTarget1.texture.dispose();
         this.summaryTarget2.texture.dispose();
     }
+}
+
+
+
+
+export class WaterObject extends EngineObject {
+    
+    private fluidSim: RungeKuttaRenderer;
+
+    constructor(renderer: WebGLRenderer, w: number, h: number, huvData: Float32Array) {
+
+        const fluidShader = `
+            float h   = data[0];
+            float u   = data[1];
+            float v   = data[2];
+            float hpx = data_px[0];
+            float upx = data_px[1];
+            float vpx = data_px[2];
+            float hmx = data_mx[0];
+            float umx = data_mx[1];
+            float vmx = data_mx[2];
+            float hpy = data_py[0];
+            float upy = data_py[1];
+            float vpy = data_py[2];
+            float hmy = data_my[0];
+            float umy = data_my[1];
+            float vmy = data_my[2];
+
+            float H  = 15.0;
+            float dx = 0.05;
+            float dy = 0.05;
+            float f = 0.001;
+            float b = 0.003;
+            float g = 9.831;
+
+            float dudx = (upx - umx) / (2.0 * dx);
+            float dvdy = (vpy - vmy) / (2.0 * dy);
+            float dhdx = (hpx - hmx) / (2.0 * dx);
+            float dhdy = (hpy - hmy) / (2.0 * dy);
+
+            float dhdt =      - H * ( dudx + dvdy );
+            float dudt = ( + f*v - b*u - g * dhdx );
+            float dvdt = ( - f*u - b*v - g * dhdy );
+
+            gl_FragColor = vec4(dhdt, dudt, dvdt, 1.0);
+        `;
+        const fluidSim = new RungeKuttaRenderer(renderer, w, h, huvData, fluidShader);
+
+
+
+        const extendedPhongShader = `
+            uniform sampler2D huvData;
+
+			#define PHONG
+            varying vec3 vViewPosition;
+            #include <common>
+            #include <uv_pars_vertex>
+            #include <uv2_pars_vertex>
+            #include <displacementmap_pars_vertex>
+            #include <envmap_pars_vertex>
+            #include <color_pars_vertex>
+            #include <fog_pars_vertex>
+            #include <normal_pars_vertex>
+            #include <morphtarget_pars_vertex>
+            #include <skinning_pars_vertex>
+            #include <shadowmap_pars_vertex>
+            #include <logdepthbuf_pars_vertex>
+            #include <clipping_planes_pars_vertex>
+
+            void main() {
+                #include <uv_vertex>
+                #include <uv2_vertex>
+                #include <color_vertex>
+
+
+                // # include <beginnormal_vertex> ---------------------------------------------------------------------------------------------------------
+
+                vec2 deltaX_ = vec2(0.1, 0.0);
+                vec2 deltaY_ = vec2(0.0, 0.1);
+
+                float h_mx = texture2D( huvData, uv - deltaX_ ).x;
+                float h_px = texture2D( huvData, uv + deltaX_ ).x;
+                float h_my = texture2D( huvData, uv - deltaY_ ).x;
+                float h_py = texture2D( huvData, uv + deltaY_ ).x;
+
+                        vec3 objectNormal = vec3(
+                            h_mx - h_px,
+                            h_my - h_py,
+                            1.0
+                );
+                        //-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+                #include <morphnormal_vertex>
+                #include <skinbase_vertex>
+                #include <skinnormal_vertex>
+                #include <defaultnormal_vertex>
+                #include <normal_vertex>
+
+
+                //# include <begin_vertex> -------------------------------------------------------------------------------------------------------------------
+                        float heightValue = texture2D( huvData, uv ).x;
+                        vec3 transformed = vec3( position.x, position.y, heightValue );
+                        //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+                #include <morphtarget_vertex>
+                #include <skinning_vertex>
+                #include <displacementmap_vertex>
+                #include <project_vertex>
+                #include <logdepthbuf_vertex>
+                #include <clipping_planes_vertex>
+                vViewPosition = - mvPosition.xyz;
+                #include <worldpos_vertex>
+                #include <envmap_vertex>
+                #include <shadowmap_vertex>
+                #include <fog_vertex>
+            }
+        `;
+        const extendedPhongMaterial = new ShaderMaterial({
+            uniforms: UniformsUtils.merge([
+                ShaderLib['phong'].uniforms, { "huvData": { value: null } }
+            ]),
+            vertexShader: extendedPhongShader,
+            fragmentShader: ShaderChunk['meshphong_frag'],
+            lights: true,
+        });
+
+        extendedPhongMaterial.uniforms["diffuse"].value = new Color(0x0040C0);
+        extendedPhongMaterial.uniforms["specular"].value = new Color(0x111111);
+        extendedPhongMaterial.uniforms["shininess"].value = 1.0;
+        extendedPhongMaterial.uniforms["opacity"].value = 1.0;
+        extendedPhongMaterial.uniforms["huvData"].value = fluidSim.getOutputTexture();
+
+        const plane = new Mesh(
+            new PlaneBufferGeometry(5, 5, 100, 100),
+            extendedPhongMaterial
+        );
+        plane.position.set(0, 0, 0);
+        plane.lookAt(0, 1, 0);
+
+        super(plane);
+
+        this.fluidSim = fluidSim;
+    }
+
+    update(time: number): void {
+        this.fluidSim.update();
+    }
+
+    handleClick(evt: any) {
+        // container.addEventListener('click', (evt) => {
+        //     plane.material.uniforms['huvData'].value = fluidSim.updateInputTexture(huv1Data);
+        //     plane.material.needsUpdate = true;
+        // });
+    }
+
 }

69c7ee0bfc83f3a4cfb043d145d460b8c9372f5d
Michael Langbein <michael_langbein@hotmail.de>
2023-03-30 10:52:57 +0200

    aggregating data works

--- h3/ingestion/main.py
+++ h3/ingestion/main.py
@@ -5,6 +5,7 @@ import rasterio as rio
 import matplotlib.pyplot as plt
 import pyproj.transformer as pt
 import os
+import numpy as np
 
 #%%
 thisDir = os.getcwd()
@@ -12,13 +13,13 @@ dataDir = os.path.join(thisDir, '..', 'data')
 fileName = "heatmap_clipped.tif"
 fileFullPath = os.path.join(dataDir, fileName)
 
-# %%
+# %% reading data
 fh = rio.open(fileFullPath)
 data = fh.read(1)
 
-#%%
+#%% parsing raw data into cells
 
-rows = []
+rows12 = []
 transformer = pt.Transformer.from_proj(fh.crs, 'EPSG:4326')
 res0 = 12
 
@@ -28,12 +29,24 @@ for r, row in enumerate(data):
         x, y = fh.xy(r, c)
         lat, lon = transformer.transform(x, y)
         cell = h3.latlng_to_cell(lat, lon, res0)
-        rows.append((cell, res0, val))
+        rows12.append((cell, val))
 
 
-# %%
-queryString = "insert into public.heat (index, resolution, value) values "
-rowStrings = [f"('{row[0]}', {row[1]}, {row[2]})" for row in rows]
+# %% inserting data into lowest resolution table
+
+"""
+CREATE TABLE IF NOT EXISTS public.heat12 (
+    index bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    h3index h3index NOT NULL,
+    "time" date,
+    value integer,
+    CONSTRAINT heat12_pkey PRIMARY KEY (index)
+)
+"""
+
+
+queryString = "insert into public.heat12 (h3index, time, value) values "
+rowStrings = [f"('{row[0]}', TO_DATE('30/03/2023', 'DD/MM/YYYY'), {row[1]})" for row in rows12]
 valuesString = ", ".join(rowStrings)
 queryString += valuesString
 queryString += ";"
@@ -49,6 +62,45 @@ conn = p2.connect(
 cursor = conn.cursor()
 cursor.execute(queryString)
 conn.commit()
+
+
+#%% filling parent-tables with aggregated data
+queryString = """
+insert into heat11 (h3index, time, value)
+select slct.pidx as h3index, TO_DATE('30/03/2023', 'DD/MM/YYYY') as time, slct.val as value
+from (
+	select round(avg(h12.value)) as val, h3_cell_to_parent(h12.h3index) as pidx
+	from heat12 as h12
+	group by pidx
+) as slct;
+"""
+cursor.execute(queryString)
+conn.commit()
+
+queryString = """
+insert into heat10 (h3index, time, value)
+select slct.pidx as h3index, TO_DATE('30/03/2023', 'DD/MM/YYYY') as time, slct.val as value
+from (
+	select round(avg(h11.value)) as val, h3_cell_to_parent(h11.h3index) as pidx
+	from heat11 as h11
+	group by pidx
+) as slct;
+"""
+cursor.execute(queryString)
+conn.commit()
+
+queryString = """
+insert into heat9 (h3index, time, value)
+select slct.pidx as h3index, TO_DATE('30/03/2023', 'DD/MM/YYYY') as time, slct.val as value
+from (
+	select round(avg(h10.value)) as val, h3_cell_to_parent(h10.h3index) as pidx
+	from heat10 as h10
+	group by pidx
+) as slct;
+"""
+cursor.execute(queryString)
+conn.commit()
+
+#%%
 conn.close()
 
-# %%

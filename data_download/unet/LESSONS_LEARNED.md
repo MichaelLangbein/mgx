3070f6f2809db99e147d61af71f507c54dfa9464
Michael Langbein <michael_langbein@hotmail.de>
2023-04-03 14:11:38 +0200

    now working forest detector

--- data_download/unet/LESSONS_LEARNED.md
+++ data_download/unet/LESSONS_LEARNED.md
@@ -6,4 +6,39 @@
 
 - input should be normalized to [-1, 1]
 
-- 
+- logits: scales a prob from [0, 1] to [-inf, inf]. Useful if model-output is an arbitrary float, not a prob.
+
+
+
+
+Observations 03.04.2023
+
+- Hypothesis 1:
+    - it's easier to predict "is forest" or "is not forest" instead of many classes
+        - reason: 
+        - test: load only forest-part of y
+    
+- Hypothesis 2:
+    - unet always wants to predict at least one class, it cannot deal with no class being active for a pixel.
+        - reason: after 30 epochs of 30i/batch, i get predictions that are never dark
+        - test: add a 4th class "none of above" in loader
+
+- Hypothesis 3:
+    - my unet needs a smaller waist
+        - reason: i keep getting chessboard-shaped predictions. Maybe the smallest unit my net can observe is 20x20 pix, not 2x2 pix
+        - test: add new down- and up-sampling layers to net
+
+- Hypothesis 4: 
+    - water is causing issues
+        - reason: there is little water in my trainings-dataset. The net stil tries to predict water sometimes, often in the middle of forests
+
+- Hypothesis 5:
+    - a small batch size seems to help
+        - reson: small batch size = can overfit to few images. actually a good thing, because there is so much variety within one class.
+        - test: make batch-size extremely small and extremely large. compare.
+
+Consequences:
+    Let's simplify things.
+        - only one class: forest or not  <== now requires spare cce, but worked!
+        - smaller waist for unet <== did work, no longer see rects
+        - small batch-size <== did work
--- data_download/unet/train.py
+++ data_download/unet/train.py
@@ -1,7 +1,7 @@
 #%%
 import keras as k
 from unet import makeModel
-from loader import SparseLoader, OneHotLoader
+from loader import SparseLoader, OneHotLoader, TrueFalseLoader
 import os
 import matplotlib.pyplot as plt
 import random
@@ -16,14 +16,15 @@ dataSubDirs = [os.path.join(dataDir, subDir) for subDir in os.listdir(dataDir)]
 random.shuffle(dataSubDirs)
 
 H, W, C = 256, 256, 3
-N_CLASSES = 3
 N_BATCH = 32
 
 cutoffPoint = int( 0.9 * len(dataSubDirs))
 trainingDirs = dataSubDirs[:cutoffPoint]
 validationDirs = dataSubDirs[cutoffPoint:]
-trainingLoader = OneHotLoader(N_BATCH, trainingDirs, H, W, C, N_CLASSES, normalizeX=True)
-validationLoader = OneHotLoader(N_BATCH, validationDirs, H, W, C, N_CLASSES, normalizeX=True)
+trainingLoader = TrueFalseLoader(N_BATCH, trainingDirs, H, W, C, normalizeX=True)
+validationLoader = TrueFalseLoader(N_BATCH, validationDirs, H, W, C, normalizeX=True)
+# trainingLoader = OneHotLoader(N_BATCH, trainingDirs, H, W, C, N_CLASSES, normalizeX=True)
+# validationLoader = OneHotLoader(N_BATCH, validationDirs, H, W, C, N_CLASSES, normalizeX=True)
 # trainingLoader = SparseLoader(N_BATCH, trainingDirs, H, W, C, normalizeX=True)
 # validationLoader = SparseLoader(N_BATCH, validationDirs, H, W, C, normalizeX=True)
 
@@ -37,7 +38,8 @@ x.max(), x.shape, y.max(), y.shape
 
 #%%
 k.backend.clear_session()
-model = makeModel(H, W, C, N_CLASSES)
+# N_CLASSES = 2: is-forest and is-not-forest
+model = makeModel(H, W, C, 2, [32, 64, 128, 256])
 model.summary()
 
 #%% verifying model works
@@ -46,19 +48,19 @@ ySim = model.predict(x)
 
 fig, axes = plt.subplots(3, 3)
 axes[0][0].imshow(x[0] + 1.0)
-axes[0][1].imshow(ySim[0] * 255)
+axes[0][1].imshow(ySim[0, :, :, 1])
 axes[0][2].imshow(y[0] * 255)
 axes[1][0].imshow(x[1] + 1.0)
-axes[1][1].imshow(ySim[1] * 255)
+axes[1][1].imshow(ySim[1, :, :, 1])
 axes[1][2].imshow(y[1] * 255)
 axes[2][0].imshow(x[2] + 1.0)
-axes[2][1].imshow(ySim[2] * 255)
+axes[2][1].imshow(ySim[2, :, :, 1])
 axes[2][2].imshow(y[2] * 255)
 ySim.shape, y.shape
 
 # %%
 # Using 'auto'/'sum_over_batch_size' reduction type.
-cce = k.losses.CategoricalCrossentropy()
+cce = k.losses.SparseCategoricalCrossentropy(from_logits=False) #ignore_class=0 ? 
 cce(y[0], ySim[0]).numpy()
 
 
@@ -74,17 +76,18 @@ class PredictCallback(k.callbacks.Callback):
         sampleNr = np.random.randint(0, N_BATCH)
         x, y = validationLoader[batchNr]
         ySim = model.predict(np.expand_dims(x[sampleNr], 0))
-        plt.imshow((x[sampleNr] + 1))
-        plt.imshow(ySim[0], alpha=.25)
+        plt.imshow(x[sampleNr] + 1.0)
+        plt.imshow(ySim[0, :, :, 1], alpha=.4)
         plt.savefig(f"./predictions/prediction_{epoch}.png")
 
 
 
 model.compile(
     optimizer = k.optimizers.RMSprop(
-        learning_rate = 0.00001,
+        learning_rate = 0.0001,
     ),
-    loss = k.losses.CategoricalCrossentropy()
+    # loss = k.losses.CategoricalCrossentropy()
+    loss = k.losses.SparseCategoricalCrossentropy(from_logits=False)
 )
 
 callbacks = [
@@ -92,10 +95,11 @@ callbacks = [
     PredictCallback()
 ]
 
+#%%
 # Train the model, doing validation at the end of each epoch.
-model.fit(
+history = model.fit(
     trainingLoader, 
-    epochs=10,
+    epochs=20,
     steps_per_epoch=30,
     batch_size=N_BATCH,
     validation_data=validationLoader, 
@@ -110,13 +114,22 @@ ySim = model.predict(x)
 
 fig, axes = plt.subplots(3, 3)
 axes[0][0].imshow(x[0] + 1.0)
-axes[0][1].imshow(ySim[0] * 255)
+axes[0][1].imshow(ySim[0, :, :, 0])
 axes[0][2].imshow(y[0] * 255)
 axes[1][0].imshow(x[1] + 1.0)
-axes[1][1].imshow(ySim[1] * 255)
+axes[1][1].imshow(ySim[1, :, :, 0])
 axes[1][2].imshow(y[1] * 255)
 axes[2][0].imshow(x[2] + 1.0)
-axes[2][1].imshow(ySim[2] * 255)
+axes[2][1].imshow(ySim[2, :, :, 0])
 axes[2][2].imshow(y[2] * 255)
 ySim.shape, y.shape
+
+# %%
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 # %%

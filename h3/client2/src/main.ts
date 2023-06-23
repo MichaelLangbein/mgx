6b7ec26b51e739894e843a44803a7bc496859c57
Michael Langbein <michael_langbein@hotmail.de>
2023-06-23 15:28:19 +0200

    reprojected to web-mercator

--- h3/client2/src/main.ts
+++ h3/client2/src/main.ts
@@ -12,8 +12,10 @@ import 'chartjs-adapter-moment';
 import { FeatureLike } from 'ol/Feature';
 import Style from 'ol/style/Style';
 import Fill from 'ol/style/Fill';
-import * as d3color from 'd3-color';
+import { fromLonLat } from 'ol/proj';
+import { colorScale } from './graph';
 
+const mapProjection = 'EPSG:3857';
 
 const baseLayer = new TileLayer({
   source: new OSM()
@@ -46,7 +48,7 @@ baseLayer.on('postrender', (event) => {
 
 const dataLayer = new VectorLayer({
   source: new VectorSource({
-    features: new GeoJSON().readFeatures(data),
+    features: new GeoJSON({ featureProjection: mapProjection }).readFeatures(data),
   }),
   style: (feature) => {
     const timeSeries = getTimeSeries(feature);
@@ -63,10 +65,11 @@ const dataLayer = new VectorLayer({
 
 const layers = [baseLayer, dataLayer];
 
+
 const view = new View({
-  center: [11.3, 48.08],
+  center: fromLonLat([11.3, 48.08], mapProjection),
   zoom: 13,
-  projection: 'EPSG:4326'
+  projection: mapProjection
 });
 
 
@@ -149,78 +152,3 @@ function getTimeSeries(feature: FeatureLike) {
   return timeSeries;
 }
 
-function colorScale(timeSeries: { date: Date; value: number; }[]): { r: any; g: any; b: any; } {
-  
-  const meanTemperature = timeSeries
-      .map(d => d.value)
-      .reduce((carry, current) => carry + current, 0)
-      / timeSeries.length;
-
-  const [r, g, b] = greenVioletRangeStepwise(0, 50, meanTemperature);
-
-  return {r, g, b};
-}
-
-
-
-function greenVioletRangeStepwise(startVal: number, endVal: number, currentVal: number): [number, number, number] {
-  const degree = fraction(currentVal, startVal, endVal);
-  const rgb = scaleInterpolation(violetGreenScale2, 1.0 - degree, false);
-  return rgb;
-}
-
-function fraction(val: number, start: number, end: number, minDistance = 0.0000001): number {
-  if (Math.abs(end - start) < minDistance) {
-    console.warn(`Trying to calculate a fraction with start- and end-point very close to each other: ${end} - ${start} = ${end - start}`);
-    return 0.0; // by l'Hospital
-  }
-  return (val - start) / (end - start);
-}
-
-function scaleInterpolation(scale: Scale, value: number, smooth = true): [number, number, number] {
-  const keys = Object.keys(scale).map(k => +k).sort();  // js-objects dont keep the ordering of numeric keys as they were entered. instead it goes: integers, strings, decimals, each group sorted by first appearance, not value.
-  const colors = keys.map(k => scale[k]);
-  const nrKeys =  keys.length;
-  if (value < keys[0]) {
-    return colors[0];
-  }
-  for (let i = 0; i < nrKeys; i++) { 
-    const startKey = keys[i];
-    const endKey = keys[i+1];
-    if (startKey <= value && value < endKey) {
-      if (!smooth) {
-        return colors[i];
-      }
-      const degree = fraction(value, startKey, endKey);
-      const startColorRGB = colors[i];
-      const endColorRGB = colors[i+1];
-      const startColorHSL = d3color.hsl(d3color.rgb(... startColorRGB));
-      const endColorHSL = d3color.hsl(d3color.rgb(... endColorRGB));
-      const h = linInterpolate(startColorHSL.h, endColorHSL.h, degree);
-      const s = linInterpolate(startColorHSL.s, endColorHSL.s, degree);
-      const l = linInterpolate(startColorHSL.l, endColorHSL.l, degree);
-      const rgb = d3color.rgb(d3color.hsl(h, s, l));
-      return [Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b)];
-    }
-  }
-  return colors[nrKeys - 1];
-}
-
-function linInterpolate(startVal: number, endVal: number, degree: number): number {
-  const degreeClamped = Math.max(Math.min(degree, 1), 0);
-  const interpolated = degreeClamped * (endVal - startVal) + startVal;
-  return interpolated;
-}
-
-interface Scale {
-  [key: string]: [number, number, number];
-}
-
-const violetGreenScale2: Scale = {
-  0.2: [184, 53, 131],
-  0.35: [213, 62, 79],
-  0.5: [252, 141, 89],
-  0.7: [254, 224, 139],
-  0.8: [230, 245, 152],
-  0.9: [153, 213, 148],
-};

4b40f3ac885db8e74f771533c1b18c45f2b90e4a
Michael Langbein <michael_langbein@hotmail.de>
2023-06-25 19:31:49 +0200

    added layers and new style

--- h3/client2/src/main.ts
+++ h3/client2/src/main.ts
@@ -9,61 +9,79 @@ import { Map, Overlay, View } from 'ol';
 import data from './buildings_analyzed.geo.json';
 import Fill from 'ol/style/Fill';
 import GeoJSON from 'ol/format/GeoJSON';
-import OSM from 'ol/source/OSM';
 import Style from 'ol/style/Style';
-import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import WMTS, { optionsFromCapabilities } from 'ol/source/WMTS';
-import WMTSTileGrid from 'ol/tilegrid/WMTS';
-import WMTSCapabilitiesParser from 'ol/format/WMTSCapabilities';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Layer from 'ol/layer/Layer';
+import TileWMS from 'ol/source/TileWMS';
+import TileSource from 'ol/source/Tile';
+// import VectorTileLayer from 'ol/layer/VectorTile';
+// import VectorTile from 'ol/source/VectorTile';
+// import TopoJSON from 'ol/format/TopoJSON';
+
 
 const mapProjection = 'EPSG:3857';
+
+
+// const baseLayer = new VectorTileLayer({
+//   source: new VectorTile({
+//     // url: "http://localhost:5173/public/vectorTiles/basic.json",
+//     url: "http://localhost:5173/public/vectorTiles/{z}/{x}/{y}.pbf?flipy=true",
+//     format: new TopoJSON(),
+//   }),
+//   style: 
+// });
 
 const baseLayer = new TileLayer({
   source: new OSM()
 });
 
-function greyscale(context: any) {
-  var canvas = context.canvas;
-  var width = canvas.width;
-  var height = canvas.height;var imageData = context.getImageData(0, 0, width, height);
-  var data = imageData.data;
-  for(let i=0; i<data.length; i += 4){
-   var r = data[i];
-   var g = data[i + 1];
-   var b = data[i + 2];
-   // CIE luminance for the RGB
-   var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-   // Show white color instead of black color while loading new tiles:
-   if(v === 0.0) v=255.0;  
-   data[i+0] = v; // Red
-   data[i+1] = v; // Green
-   data[i+2] = v; // Blue
-   data[i+3] = 125; // Alpha
-  }
-  context.putImageData(imageData,0,0);
+// function greyscale(context: any) {
+//   var canvas = context.canvas;
+//   var width = canvas.width;
+//   var height = canvas.height;var imageData = context.getImageData(0, 0, width, height);
+//   var data = imageData.data;
+//   for(let i=0; i<data.length; i += 4){
+//    var r = data[i];
+//    var g = data[i + 1];
+//    var b = data[i + 2];
+//    // CIE luminance for the RGB
+//    var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+//    // Show white color instead of black color while loading new tiles:
+//    if(v === 0.0) v=255.0;  
+//    data[i+0] = v; // Red
+//    data[i+1] = v; // Green
+//    data[i+2] = v; // Blue
+//    data[i+3] = 125; // Alpha
+//   }
+//   context.putImageData(imageData,0,0);
+// }
+
+// baseLayer.on('postrender', (event) => {
+//   greyscale(event.context);
+// });
+
+
+
+const layerNames = ["ls8:lst-2020-11-17", "ls8:lst-2020-12-03", "ls8:lst-2020-12-19", "ls8:lst-2021-01-04", "ls8:lst-2021-01-20", "ls8:lst-2021-02-05", "ls8:lst-2021-02-21", "ls8:lst-2021-11-20", "ls8:lst-2021-12-14", "ls8:lst-2021-12-21", "ls8:lst-2021-12-22", "ls8:lst-2022-01-07", "ls8:lst-2022-01-15", "ls8:lst-2022-01-23", "ls8:lst-2022-02-24", "ls8:lst-2022-08-03", "ls8:lst-2022-10-06", "ls8:lst_2022-08-03"];
+const rasterLayers: Layer[] = [];
+for (const layerName of layerNames) {
+  rasterLayers.push(new TileLayer({
+    source: new TileWMS({
+      url: "http://localhost:8080/geoserver/ls8/wms",
+      params: {
+        "LAYERS": layerName,
+        "STYLES": "thermal"
+      },
+      serverType: 'geoserver'
+    })
+  }))
 }
 
-baseLayer.on('postrender', (event) => {
-  greyscale(event.context);
-});
 
-
-const wmtsCapabilitiesResponse = await fetch("http://localhost:8080/geoserver/gwc/service/wmts?service=WMTS&version=1.1.1&request=GetCapabilities");
-const wmtsCapabilitiesXml = await wmtsCapabilitiesResponse.text();
-const parser = new WMTSCapabilitiesParser();
-const parsedWMTSCapabilities = parser.read(wmtsCapabilitiesXml);
-const wmtsOptions = optionsFromCapabilities(parsedWMTSCapabilities, {
-  layer: "thermal:LC09_L1TP_193026_20230424_20230424_02_T1_B10",
-  matrixSet: "EPSG:900913"
-})!;
-
-const band10Layer = new TileLayer({
-  source: new WMTS(wmtsOptions)
-});
-
-const dataLayer = new VectorLayer({
+const vectorData = new VectorLayer({
   source: new VectorSource({
     features: new GeoJSON({ featureProjection: mapProjection }).readFeatures(data),
   }),
@@ -80,7 +98,7 @@ const dataLayer = new VectorLayer({
   }
 });
 
-const layers = [band10Layer, dataLayer];
+const layers = [baseLayer, ...rasterLayers, vectorData];
 
 
 const view = new View({
@@ -106,7 +124,7 @@ const map = new Map({
 map.on("click", (event) => {
   const coords = event.coordinate;
   let foundFeature = false;
-  dataLayer.getSource()?.forEachFeatureAtCoordinateDirect(coords, (feature) => {
+  vectorData.getSource()?.forEachFeatureAtCoordinateDirect(coords, (feature) => {
     foundFeature = true;
     popup.setPosition(coords);
     const element = popup.getElement();

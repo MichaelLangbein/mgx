df968f54b3596e6d84d19abed60ff5457bc57437
Michael Langbein <michael_langbein@hotmail.de>
2023-07-07 14:00:01 +0200

    refactored

--- h3/ingestion/main.py
+++ h3/ingestion/main.py
@@ -1,133 +1,150 @@
 #%%
 import os
 import json
-import tarfile
-
-from pyproj import CRS
-from raster import makeTransform, readTif, saveToTif, tifGetBbox
-from osm import downloadAndSaveOSM
-from download import downloadLandsat
-from analyze import bt2lstSingleWindow, emissivityFromOSM, extractClouds, lstFromFile_Avdan, radiance2BrightnessTemperature, readMetaData, scaleBandData
+import csv
 import numpy as np
+import fiona
+from shapely.geometry import shape
+import matplotlib.pyplot as plt
+
+from raster import readTif, tifGetPixelSizeDegrees, tifGetBbox
+from vectorAndRaster import rasterizePercentage
+from analyze import extractClouds, scaleBandData, radiance2BrightnessTemperature, bt2lstSingleWindow
+
+np.seterr(all="ignore")
 
 
-#%%
-dirPath = os.path.dirname(os.path.realpath(__file__))
-aoi = { "lonMin": 11.214, "latMin": 48.064, "lonMax": 11.338, "latMax": 48.117 }
-startDate = "2019-11-01"
-endDate = "2022-11-01"
+def readJson(path):
+    fh = open(path)
+    data = json.load(fh)
+    return data
 
-#%%
-# helpers
+def getMaxPixelSize(path):
+    tifFile = readTif(path)
+    sizeH, sizeW = tifGetPixelSizeDegrees(tifFile)
+    return max( sizeH, sizeW)
 
-def readGeoJson(path):
-    try:
-        fh = open(path)
-        data = json.load(fh)
-        return data
-    except:
-        return None
+def getPixelData(tifPath, bbox):
+    tifFile = readTif(tifPath)
+    lonMin, latMin, lonMax, latMax = bbox
+    return tifGetBbox(tifFile, {"lonMin": lonMin, "latMin": latMin, "lonMax": lonMax, "latMax": latMax})
 
-def extractLs8Tar(path):
-    pathExtracted = path.replace(".tar", "")
-    fileName = os.path.basename(path).replace(".tar", "")
-    fh = tarfile.open(path)
-    fh.extractall(pathExtracted)
-    fh.close()
-    return pathExtracted, fileName + "_"
+def pixelizeCoverageFraction(geometries, bbox, shape):
+    (lonMin, latMin, lonMax, latMax) = bbox
+    dbox = {"lonMin": lonMin, "latMin": latMin, "lonMax": lonMax, "latMax": latMax}
+    percentage = rasterizePercentage(geometries, dbox, shape)
+    return percentage / 100
 
-def calcBbox(points):
-    lonMin = min(point[0] for point in points)
-    latMin = min(point[1] for point in points)
-    lonMax = max(point[0] for point in points)
-    latMax = max(point[1] for point in points)
-    return {"lonMin": lonMin, "latMin": latMin, "lonMax": lonMax, "latMax": latMax}
-
-
-#%% get osm data
-osmData = {}
-try:
-    osmData = {
-        "buildings": readGeoJson(f"{dirPath}/osm/buildings.geo.json"),
-        "trees": readGeoJson(f"{dirPath}/osm/trees.geo.json"),
-        "water": readGeoJson(f"{dirPath}/osm/water.geo.json")
-    }
-except:
-    osmData = downloadAndSaveOSM(aoi, saveToDirPath=f"{dirPath}/osm", getBuildings=True, getTrees=True, getWater=True)
-
-
-#%% get ls8 data
-ls8files = [os.path.abspath(os.path.join(f"{dirPath}/ls8", f)) for f in os.listdir(f"{dirPath}/ls8") if f.endswith(".tar")]
-if len(ls8files) == 0:
-    ls8files = downloadLandsat(aoi, startDate, endDate, 50, outputDir=f"{dirPath}/ls8")
-
-
-#%%
-for ls8file in ls8files:
-    print(f"...... processing file {ls8file} .........")
-
-    pathToFile, fileNameBase = extractLs8Tar(ls8file)
-
-    base = f"{pathToFile}/{fileNameBase}"
-    # `noDataValue` must not be np.nan, because then `==` doesn't work as expected
+def estimateLst(b10, qa, meta, buildingFraction, roadFraction):
     noDataValue = -9999
+    vegetationEmissivity = 0.973
+    buildingEmissivity   = 0.932  # https://pure.tudelft.nl/ws/files/95823567/1_s2.0_S221209552100167X_main.pdf
+    roadEmissivity       = 0.945
 
-    metaData = readMetaData(base + "MTL.json")
+    b10NoClouds = extractClouds(b10, qa, noDataValue)
+    toaRadiance = scaleBandData(b10NoClouds, 10, meta)
+    noDataMask = (toaRadiance == noDataValue)
+    toaBT = radiance2BrightnessTemperature(toaRadiance, meta)
+    emissivity = buildingFraction * buildingEmissivity + roadsFraction * roadEmissivity + (1 - buildingFraction - roadsFraction) * vegetationEmissivity
+    lst = bt2lstSingleWindow(toaBT - 273, emissivity)
+    lst = np.where(noDataMask, np.nan, lst)
+    return lst
 
-    qaPixelFh        = readTif(base + "QA_PIXEL.TIF")
-    toaRadiance10Fh  = readTif(base + "B10.TIF")
-    assert(qaPixelFh.res == toaRadiance10Fh.res)
-
-    qaPixelAOI        = tifGetBbox(qaPixelFh, aoi)[0]
-    toaRadiance10AOI  = tifGetBbox(toaRadiance10Fh, aoi)[0]
-
-    toaRadiance10NoClouds = extractClouds(toaRadiance10AOI, qaPixelAOI, noDataValue)
-
-    # Converting raw scaled sensor-data to spectral radiance [W/m²]
-    toaRadiance10 = scaleBandData(toaRadiance10NoClouds, 10, metaData)
-
-    noDataMask = (toaRadiance10 == noDataValue)
-
-    # Step 1: radiance to at-sensor temperature (brightness temperature BT)
-    toaBT10 = radiance2BrightnessTemperature(toaRadiance10, metaData)
-    toaBT10 = np.where(noDataMask, noDataValue, toaBT10)
-
-    # Step 2: estimate emissivity from OSM data
-    emissivity10 = emissivityFromOSM(10, aoi, toaRadiance10.shape, osmData["buildings"], osmData["trees"])
-
-    # Step 3: black-body-temperature to land-surface-temperature
-    landSurfaceTemperature = bt2lstSingleWindow(toaBT10 - 273.15, emissivity10)
-    landSurfaceTemperature = np.where(noDataMask, np.nan, landSurfaceTemperature)
-
-    # adding projection metadata
-    imgH, imgW = landSurfaceTemperature.shape
-    transform = makeTransform(imgH, imgW, aoi)
-    saveToTif(f"{pathToFile}/lst.tif", landSurfaceTemperature, CRS.from_epsg(4326), transform, noDataValue)
-    lstTif = readTif(f"{pathToFile}/lst.tif")
+def splitAlong(data, bbox, outline):
+    outlineRasterized = pixelizeCoverageFraction([outline], bbox, data.shape)
+    inside = data * outlineRasterized
+    outside = data - inside
+    return inside, outside 
 
 
-    for building in osmData["buildings"]["features"]:
-        
-        props = building["properties"]
-        geom  = building["geometry"]["coordinates"][0]
 
-        buildingBbox = calcBbox(geom)
 
-        try:
-            pixelData = tifGetBbox(lstTif, buildingBbox)
-            meanT = np.mean(pixelData)
+pathToLs8Data          = "./ls8"
+pathToOsmDataBuildings = "./osm/buildings.geo.json"
+pathToOsmDataRoads     = "./osm/roads.geo.json"
 
-            if "timeseries" not in props:
-                props["timeseries"] = {}
+
+buildingData = fiona.open(pathToOsmDataBuildings)
+roadData = fiona.open(pathToOsmDataRoads)
+scenes = []
+for dir in os.listdir(pathToLs8Data):
+    if dir[-3:] != "tar":
+        scenes.append({
+            "b10":   f"{pathToLs8Data}/{dir}/{dir}_B10.TIF",
+            "qa":    f"{pathToLs8Data}/{dir}/{dir}_QA_PIXEL.TIF",
+            "meta":  f"{pathToLs8Data}/{dir}/{dir}_MTL.json"
+        })
+distance = 2 * getMaxPixelSize(scenes[0]["b10"])
+roadSize = 0.01 * distance
+
+
+with open("deltaTs.csv", "w") as dest:
+    fields = ["buildingId"] + [p for p in os.listdir(pathToLs8Data) if p[-3:] != "tar"]
+    writer = csv.DictWriter(dest, fieldnames=fields)
+    writer.writeheader()
+
+    buildings = [bld for bld in buildingData]
+    for i, building in enumerate(buildings):
+        print(f"... {100 * i / len(buildings)}% ...")
+        if i > 500:
+            break
+
+        buildingGeometry = building["geometry"]
+        shp              = shape(buildingGeometry)
+        outline          = shp.exterior
+        boutline         = shp.buffer(distance)
+        bbox             = boutline.bounds
+
+        neighboringBuildings = [b for b in buildingData.filter(bbox=bbox)]
+        neighborGeometries   = [b["geometry"] for b in neighboringBuildings]
+        neighborRoads        = [shape(r["geometry"]).buffer(roadSize) for r in roadData.filter(bbox=bbox)]
+
+        data = {}
+        for scene in scenes:
+            meta                  = readJson(scene["meta"])
+            b10                   = getPixelData(scene["b10"], bbox)[0]
+            qa                    = getPixelData(scene["qa"], bbox)[0]
             
-            props["timeseries"][fileNameBase] = meanT
+            housesFraction        = pixelizeCoverageFraction(neighborGeometries, bbox, b10.shape)
+            roadsFraction         = pixelizeCoverageFraction(neighborRoads, bbox, b10.shape)
+            lst                   = estimateLst(b10, qa, meta, housesFraction, roadsFraction)
+            
+            buildingFraction      = pixelizeCoverageFraction([buildingGeometry], bbox, b10.shape)
+            buildingFractionNorm  = buildingFraction / np.sum(buildingFraction)
+            nrHouses              = 1
+            nrNonHouses           = buildingFractionNorm.size - nrHouses
+            tMeanInside           = np.sum(lst * buildingFractionNorm) / nrHouses
+            tMeanOutside          = np.sum(lst * (1.0 - buildingFractionNorm)) / nrNonHouses
+            deltaT                = tMeanInside - tMeanOutside
 
-            print(f"temp: {meanT}")
-        except Exception as e:
-            print(f"error {e}")
+            if not np.isnan(deltaT):
+                data[meta["LANDSAT_METADATA_FILE"]["PRODUCT_CONTENTS"]["LANDSAT_PRODUCT_ID"]] = deltaT
+
+        data["buildingId"] = building.id
+        writer.writerow(data)
+
+
+print("Done!")
 
 # %%
+f = open("./osm/buildings.geo.json")
+jsondata = json.load(f)
+f = open("./deltaTs.csv")
+reader = csv.DictReader(f)
 
-with open("./buildings_analyzed.geo.json", "w") as fh:
-    fh.writelines(json.dumps(osmData["buildings"], indent=4))
+def find(arr, func):
+    for el in arr:
+        if func(el):
+            return el
+
+for row in reader:
+    id = row["buildingId"]
+    feature = find(jsondata["features"], lambda f: f["properties"]["id"] == id)
+    if feature:
+        feature["properties"]["deltaTs"] = row
+
+
+f = open("./buildings.geojson", "w")
+json.dump(jsondata, f)
+    
 # %%
